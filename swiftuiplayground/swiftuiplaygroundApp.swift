48f1cf14130e69aad4e6519c5c7db19a171e4f85
masaki nakamura <gmk4n0n@gmail.com>
2022-12-16 07:59:49 +0900

    app lifecycle SwiftUI

--- swiftuiplayground/swiftuiplaygroundApp.swift
+++ swiftuiplayground/swiftuiplaygroundApp.swift
@@ -9,9 +9,33 @@ import SwiftUI
 
 @main
 struct swiftuiplaygroundApp: App {
+    
+    @Environment(\.scenePhase) var scenePhase
+
+    init() {
+        firstExec()
+    }
+    
     var body: some Scene {
         WindowGroup {
             ListMainView()
+        }.onChange(of: scenePhase) { (phase) in
+            switch phase {
+                // app lifecycle
+            case .active:
+                print("It's active")
+            case .inactive:
+                print("It's inactive")
+            case .background:
+                print("It's in background")
+            @unknown default:
+                fatalError("somethins went wrong")
+            }
+            
         }
+    }
+    
+    func firstExec() {
+        print("first to be executed")
     }
 }
